6402a8a5606e77128cfb8e27652b5b858cbaa14b
joaor <joaor@DESKTOP-ORTRKJA>
2018-08-05 00:56:23 -0300

    Criado 02_THREAD

---
+++ 02_Thread/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t = new Thread(ThreadRepeticao);
+
+                t.Start();
+            }
+
+            Console.WriteLine("###################Finalizado o programa #####################");
+            Console.ReadKey();
+
+        }
+
+        static void ThreadRepeticao()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.WriteLine("Repetição: " + i);
+            }
+        }
+    }
+}
