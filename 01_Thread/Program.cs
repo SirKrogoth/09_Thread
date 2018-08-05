f3810b589d8ca9bc8c69537f381779d5ec126b6b
joaor <joaor@DESKTOP-ORTRKJA>
2018-08-04 23:39:54 -0300

    Primeiro programa

--- 01_Thread/Program.cs
+++ 01_Thread/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace _01_Thread
 {
@@ -10,6 +11,25 @@ namespace _01_Thread
     {
         static void Main(string[] args)
         {
+            Thread t = new  Thread(ThreadRepeticao);
+
+            t.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Main: " + i);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static void ThreadRepeticao()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Repetição: " + i);
+            }
         }
     }
 }
